bce6d2be9568600a16c97895c402f7d119d6b8f3
mutcato <mtokat1@gmail.com>
2021-10-17 21:49:05 +0300

    Add SQLite eraser according to TTL

--- TTL_erase_rows.sh
+++ TTL_erase_rows.sh
@@ -4,4 +4,5 @@
 # Deletes rows from technical_data table which is in coinmove.db Sqlite database
 # It deletes according to ttl (time to live) column
 # TTL column represents end of lifetime of that row
-echo "DELETE FROM technical_data WHERE ttl<" $(date +%s) | sqlite3 coinmove.db
+echo "DELETE FROM technical_data WHERE ttl<" $(date +%s) | sqlite3 /home/pi/Public/coinmove/technical-coin-data-scraper/coinmove.db
+echo "SELECT COUNT(*) FROM technical_data;" | sqlite3 /home/pi/Public/coinmove/technical-coin-data-scraper/coinmove.db
